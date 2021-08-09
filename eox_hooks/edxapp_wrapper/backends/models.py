06707e4dcbe496264973cea5dda3848ae132e3ee
mariagrimaldi <maria.grimaldi@edunext.co>
2021-08-09 11:03:53 -0400

    fix: fix pylint errors

--- eox_hooks/edxapp_wrapper/backends/models.py
+++ eox_hooks/edxapp_wrapper/backends/models.py
@@ -1,13 +1,14 @@
 """
 Simple backend that returns the platform's models
 """
+# pylint: disable=import-error,import-outside-toplevel
 from django.conf import settings
 
 
 def get_openedx_certificate_model():
     """Return the GeneratedCertificate model class when called during runtime"""
     if not getattr(settings, "EOX_HOOKS_TEST_ENVIRONMENT", False):
-        from lms.djangoapps.certificates.models import GeneratedCertificate  # pylint: disable=import-error
+        from lms.djangoapps.certificates.models import GeneratedCertificate
 
         return GeneratedCertificate
     # Return None when testing to avoid ImportError
@@ -17,7 +18,7 @@ def get_openedx_certificate_model():
 def get_openedx_user_profile_model():
     """Return the UserProfile model class when called during runtime"""
     if not getattr(settings, "EOX_HOOKS_TEST_ENVIRONMENT", False):
-        from student.models import UserProfile  # pylint: disable=import-error
+        from student.models import UserProfile
 
         return UserProfile
     # Return None when testing to avoid ImportError
