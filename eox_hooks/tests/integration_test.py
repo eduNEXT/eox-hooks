b9b7501f065772ee7e1b9f4a3ec386eb3435fac5
Luis Felipe Castano <felipe.castano@edunext.co>
2024-05-10 14:19:56 -0500

    fix: ignore pylint integration file

--- eox_hooks/tests/integration_test.py
+++ eox_hooks/tests/integration_test.py
@@ -1,28 +1,20 @@
 """
 Test integration file.
 """
-from unittest.mock import MagicMock, patch  # pylint: disable=C0411
-
 from django.test import TestCase
 
 
-class TutorIntegrationTestCase(TestCase):
+class IntegrationTestCase(TestCase):
     """
     Tests integration with openedx
     """
-
 
     # pylint: disable=import-outside-toplevel,unused-import
     def test_current_settings_code_imports(self):
         """
         Running this imports means that our backends import the right signature
         """
-        import eox_hooks.edxapp_wrapper.backends.course_modes_j_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.course_modes_l_v1  # isort:skip
-        import eox_hooks.edxapp_wrapper.backends.courses_j_v1  # isort:skip
-        import eox_hooks.edxapp_wrapper.backends.courses_l_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.courses_p_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.enrollments_l_v1  # isort:skip
-        import eox_hooks.edxapp_wrapper.backends.enrollments_j_v1  # isort:skip
-        import eox_hooks.edxapp_wrapper.backends.models_j_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.models_l_v1  # isort:skip
