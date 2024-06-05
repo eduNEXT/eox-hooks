919440ce8a45fb42cb4c338a29aa6060bf7fd8fe
Luis Felipe Castano <felipe.castano@edunext.co>
2024-06-05 18:26:21 -0500

    fix: quality changes

--- eox_hooks/tests/tutor/integration.sh
+++ eox_hooks/tests/tutor/integration.sh
@@ -2,10 +2,10 @@
 
 # This script installs the package in the edxapp environment, installs test requirements from Open edX and runs the tests using the Tutor settings
 echo "Install package"
-pip install -e $(pwd)/eox-hooks
+pip install -e /openedx/eox-hooks
 
 echo "Install test-requirements"
 make test-requirements
 
 echo "Run tests"
-pytest -s --ds=lms.envs.tutor.test $(pwd)/eox-hooks/eox_hooks/tests/tutor
+pytest -s --ds=lms.envs.tutor.test /openedx/eox-hooks/eox_hooks/tests/tutor
