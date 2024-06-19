fcc38de5ad675ab9eb0e330ce30a05f7ba586565
Luis Felipe Castano <felipe.castano@edunext.co>
2024-06-19 01:40:35 -0500

    feat: add api test

--- eox_hooks/tests/tutor/integration_test_tutor.py
+++ eox_hooks/tests/tutor/integration_test_tutor.py
@@ -1,13 +1,20 @@
 """
 Test integration file.
 """
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
 
 class TutorIntegrationTestCase(TestCase):
     """
     Tests integration with openedx
     """
+
+    @override_settings(ALLOWED_HOSTS=['local.edly.io'])
+    def setUp(self):
+        """
+        Set up the base URL for the tests
+        """
+        self.base_url = 'http://local.edly.io'
 
     # pylint: disable=import-outside-toplevel,unused-import
     def test_current_settings_code_imports(self):
@@ -18,3 +25,21 @@ class TutorIntegrationTestCase(TestCase):
         import eox_hooks.edxapp_wrapper.backends.courses_p_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.enrollments_l_v1  # isort:skip
         import eox_hooks.edxapp_wrapper.backends.models_l_v1  # isort:skip
+
+    def test_info_view(self):
+        """
+        Tests the info view endpoint in Tutor
+        """
+        info_view_url = f'{self.base_url}/eox-hooks/eox-info/'
+
+        # Simulate a GET request to the info endpoint using the full URL
+        response = self.client.get(info_view_url)
+
+        # Verify the response status code
+        self.assertEqual(response.status_code, 200)
+
+        # Verify the response format
+        response_data = response.json()
+        self.assertIn('version', response_data)
+        self.assertIn('name', response_data)
+        self.assertIn('git', response_data)
